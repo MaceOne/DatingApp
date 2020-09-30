a445657703bb3e72b27d3236599dc7662ead4ce5
Mads Dahl Dalsgaard <madsd77@gmail.com>
2020-09-30 11:37:19 +0200

    End of Section 28

---
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -0,0 +1,22 @@
+import { catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { AlertifyService } from '../_sevices/alertify.service';
+import { UserService } from '../_sevices/user.service';
+import { User } from '../_models/user';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
+
+@Injectable()
+export class MemberListResolver implements Resolve<User[]> {
+    constructor(private userService: UserService, private router: Router, private alert: AlertifyService){}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
+        return this.userService.getUsers().pipe(
+            catchError(error => {
+                this.alert.error('Problem retriving data');
+                this.router.navigate(['/home']);
+                return of(null);
+            })
+        );
+    }
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,22 +1,34 @@
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ErrorInterceptorProvider } from './_sevices/error.interceptor';
 import { AuthService } from './_sevices/auth.service';
 import { appRoutes } from './routes';
+import { AlertifyService } from 'src/app/_sevices/alertify.service';
+import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
@@ -26,7 +38,9 @@ import { appRoutes } from './routes';
       RegisterComponent,
       MemberListComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
@@ -34,11 +48,24 @@ import { appRoutes } from './routes';
       FormsModule,
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      TabsModule.forRoot(),
+      RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
+      JwtModule.forRoot({
+         config: {
+            tokenGetter,
+            allowedDomains: ['localhost:5000'],
+            disallowedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       ErrorInterceptorProvider,
-      AuthService
+      AuthService,
+      AuthGuard,
+      AlertifyService,
+      MemberDetailResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
